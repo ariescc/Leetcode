80ff89e5c922a9be039ce79c74e7e020233ce130
ariescc <jiachang@mail.nwpu.edu.cn>
2017-11-09 21:53:00 +0800

    605. Can Place Flowers ac

--- C++/605.cpp
+++ C++/605.cpp
@@ -4,18 +4,28 @@ public:
         
 		if(n == 0) return true;
 		
+		int cnt = 0;
+		
 		for(int i = 0; i < flowerbed.size(); i++)
 		{
-			if(flowerbed[i-1] == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0)
+			if(i == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0) 
 			{
 				flowerbed[i] = 1;
-				n--;
-				if(n == 0) return true;
+				cnt++;
 			}
-			
-		}	
-
-		return false;
-		
+			else if(i != 0 && i != flowerbed.size()-1 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0
+				&& flowerbed[i] == 0) 
+			{
+				flowerbed[i] = 1;
+				cnt++;
+			}
+			else if(i == flowerbed.size()-1 && flowerbed[i-1] == 0 && flowerbed[i] == 0)
+			{
+				flowerbed[i] = 1;
+				cnt++;
+			}
+		}
+		if(cnt >= n) return true;
+		else return false;
     }
 };
