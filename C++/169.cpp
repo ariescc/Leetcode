623117430f921d286659a18b33b06667ab230516
ariescc <jiachang@mail.nwpu.edu.cn>
2017-11-14 11:17:12 +0800

    169. Majority Element ac

---
+++ C++/169.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+        }
+        for(int i = 0; i < nums.size() / 2 + 1; i++) {
+            if(mp[nums[i]] > nums.size() / 2) return nums[i];
+        }
+    }
+};
