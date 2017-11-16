31675c45d6eeaf0e8f5ee69af91fac04347e6549
ariescc <jiachang@mail.nwpu.edu.cn>
2017-11-16 14:40:29 +0800

    没意义，pass

--- C++/189.cpp
+++ C++/189.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int len = nums.size() - k;
-        reverse(nums.begin(), nums.begin() + len);
-        reverse(nums.begin() + len + 1, nums.end());
-        vector<int> tmp1, tmp2;
-        copy(nums.begin(), nums.begin() + len, tmp2.begin());
-        copy(nums.begin() + len + 1, nums.end(), tmp1.begin());
-        copy(tmp1.begin(), tmp1.begin() + nums.end() - num.begin() - len, nums.begin());
-        copy(tmp2.begin(), tmp2.begin() + len, nums.begin() + nums.end() - nums.begin() - len + 1);
+        if(k <= nums.size()) {
+            reverse(nums.begin(), nums.begin() + nums.size() - k);
+            reverse(nums.begin() + nums.size() - k , nums.end());
+            reverse(nums.begin(), nums.end());
+        }
+        else {
+            reverse(nums.begin(), nums.end());
+        }
     }
 };
