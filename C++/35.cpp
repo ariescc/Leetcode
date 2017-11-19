0d3fd7eb76d90a80fc46092b44beb616e58df245
ariescc <jiachang@mail.nwpu.edu.cn>
2017-11-19 11:00:52 +0800

    35. Search Insert Position ac

---
+++ C++/35.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+        if(target > nums[nums.size() - 1]) return nums.size();
+        else if(target <= nums[0]) return 0;
+
+        for(int i = 0; i < nums.size() - 1; i++) {
+            if(target > nums[i] && target <= nums[i+1]) return i + 1;
+        }
+    }
+};
