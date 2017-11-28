6ddb2302033f5377f280f70c972c2025d605cbfc
ariescc <jiachang@mail.nwpu.edu.cn>
2017-11-28 20:43:21 +0800

    541. Reverse String II ac

--- C++/541.cpp
+++ C++/541.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     string reverseStr(string s, int k) {
-        int cnt = s.length() / (2 * k);
-        int lef = s.length() % (2 * k);
-        if(cnt > 0) {
-            for(int i = 1; i <= cnt; i++) {
-                reverse(s.begin()+(i-1)*2*k, s.begin()+(i-1)*2*k + k - 1);
+        int pos = 0;
+        while(pos < s.size()) {
+            if(pos + k > s.size()) {
+                k = s.size() - pos;
             }
-        }
-        if(lef >= k) {
-            reverse(s.end() - lef + 1, s.end() - lef + k);
-        }
-        else {
-            reverse(s.end() - lef + 1, s.end());
+            string tmpStr = s.substr(pos, k);
+            reverse(tmpStr.begin(), tmpStr.end());
+            s.replace(pos, k, tmpStr);
+            pos += 2 * k;
         }
         return s;
     }
